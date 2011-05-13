24b3269e276a7b619dfda35e1c3f7c909769d5ec
Scott Sayles <scott.sayles@gmail.com>
2011-05-12 22:47:25 -0400

    adding aliases for screen

--- etc/aliases.sh
+++ etc/aliases.sh
@@ -6,3 +6,7 @@ alias ll='ls -lG'
 
 # for those pesky environments that push vi on you when they have vim!
 [[ -n "`type -P vim`" ]] && alias vi='vim'
+
+
+alias scr='screen'
+alias scrh='screen -h 100000'
