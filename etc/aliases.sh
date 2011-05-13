eceb088536265c596cceeed146b90068e99a2949
Scott Sayles <scott.sayles@gmail.com>
2011-05-12 22:49:54 -0400

    adding alias for tail -f and resorting by alphabetic

--- etc/aliases.sh
+++ etc/aliases.sh
@@ -1,12 +1,16 @@
 # general aliases
 
-alias ls='ls -G'
 alias ll='ls -lG'
+alias ls='ls -G'
+
+
+alias scr='screen'
+alias scrh='screen -h 100000'
+
+
+alias tlf='tail -f'
 
 
 # for those pesky environments that push vi on you when they have vim!
 [[ -n "`type -P vim`" ]] && alias vi='vim'
 
-
-alias scr='screen'
-alias scrh='screen -h 100000'
