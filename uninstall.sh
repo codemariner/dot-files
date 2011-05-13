81226f7d95b9c5c8db1c8f3f409bb6f066bb2daa
Scott Sayles <scott.sayles@gmail.com>
2011-05-12 23:54:02 -0400

    adding an uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+START_DIR=`pwd`
+
+# get absolute path to the top level directory
+cd `dirname $0`
+ROOT_DIR=`pwd`
+
+trap 'echo "uninstall completed!"' 0
+
+echo "Removing links..."
+# unlink all files
+
+function remove_links_from_dir() {
+    local dir_name=$1
+    cd ~/$dir_name
+    echo "[~/$dir_name]"
+
+    for file in `ls -a`; do
+        # go back to where we were (on subsequent iterations)
+        cd ~/$dir_name
+
+        # skip it if it's not a link
+        if [ ! -L $file ]; then
+            continue;
+        fi
+
+        dest=`readlink $file`
+        if [ -f $dest ]; then
+            filename=`basename $dest`
+            # go to the directory where that file is so we can figure out
+            # where it is exactly
+            cd `dirname $dest`
+            if [ $ROOT_DIR/$dir_name -ef `pwd` ]; then
+                echo "  rm ~/$dir_name/$filename"
+                rm ~/$dir_name/$filename
+            fi
+        elif [ -d $file ]; then
+                echo "  rm ~/$dir_name/$file"
+                rm ~/$dir_name/$file
+        fi
+    done
+}
+
+echo ""
+
+remove_links_from_dir bin
+remove_links_from_dir etc
+remove_links_from_dir .
+
+echo ""
