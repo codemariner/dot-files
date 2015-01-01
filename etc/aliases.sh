c4268e9c0a856c730d9d715305b093ac43a5ef13
Scott Sayles <scott.sayles@gmail.com>
2015-01-01 16:08:25 -0500

    Add 'serve' alias for serving current directory via python http server.

--- etc/aliases.sh
+++ etc/aliases.sh
@@ -14,3 +14,5 @@ alias tlf='tail -f'
 # for those pesky environments that push vi on you when they have vim!
 [[ -n "`type -P vim`" ]] && alias vi='vim'
 
+
+alias serve='python -m SimpleHTTPServer'
